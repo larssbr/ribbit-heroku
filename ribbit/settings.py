4ecd906eeff5482142911e3eb573bace9b05818a
Lars Brusletto <larsbrusletto@gmail.com>
2014-08-24 22:07:18 +0200

    Database name error works now

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -28,7 +28,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config() # must be after the big "DATABASES"
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
