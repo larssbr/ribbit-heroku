b6bb8d73bd2198e15baefc91f784755d36e7ff64
Lars Brusletto <larsbrusletto@gmail.com>
2014-08-25 13:05:52 +0200

    fixed procfile
    web: gunicorn <directory_containing_settings.py_file>.wsgi

---
+++ ribbit/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/larsbrusletto/Documents/Programing/ribbit-heroku/ribbit/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
