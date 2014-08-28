05837c3a7a8e2964b6ecc33d712ae96a37638e4a
Lars Brusletto <larsbrusletto@gmail.com>
2014-08-28 11:31:43 +0200

    Tester

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -30,7 +30,7 @@ DATABASES = {
     }
 }
 
-#DATABASES['default'] = dj_database_url.config() 
+#DATABASES['default'] = dj_database_url.config()  # postgres://USER:PASSWORD@HOST:PORT/NAME
 DATABASES['default'] =  dj_database_url.config(default="postgres://postgres:123@localhost:5432/test") # must be after the big "DATABASES"
 
 # Local time zone for this installation. Choices can be found here:
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/larsbrusletto/Programing/ribbit-heroku/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
